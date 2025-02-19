09d844b30e8b33bf8a752dce895730910ff725c5
Sanketkavanekar <sanketkavanekar1125@gmail.com>
2025-02-19 13:22:37 +0530

    Add files via upload

--- Question11.java
+++ Question11.java
@@ -1,23 +1,23 @@
 import java.util.*;
 class Question11{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);    
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        sc.close();
-        if(a>b && a>c){
-            System.out.println("Largest number is  "+a);
-        }
-        else if(b>a && b>c){
-            System.out.println("Largest element is"+b);
+    int number;
+    Scanner st=new Scanner(System.in);
+    number=st.nextInt();
+    int result;
+    while(number>0){
+       result= number%10;     //appending remainder to result variable
+       
+       number=number/10;   //it will divide until quotient becomes less than n.
+       System.out.print(result);
+}
+    
+    
+
 
 }
-        else{
-            System.out.println("Largest number is "+c);
 
-}
-  
 
- }
+
+
 }
